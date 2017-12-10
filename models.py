3a9f91fb0156bb61842af37bce1702ea658d745b
Lorenzo Terenzi <lorenzo.terenzi@lumos-dev.com>
2017-12-10 16:10:57 +0100

    added single layer and linear model

--- models.py
+++ models.py
@@ -22,3 +22,38 @@ class BCEModel(nn.Module):
 
     def add_optimizer(self):
         self.optimizer = optim.Adam(self.parameters(), lr=self.learning_rate)
+
+class LinearBCEModel(nn.Module):
+    def __init__(self, embedding_dims, learning_rate=10**-3):
+        super().__init__()
+        self.model = nn.Sequential(
+            nn.Linear(embedding_dims, 1024),
+            nn.Sigmoid())
+        self.learning_rate = learning_rate
+        self.loss = nn.BCELoss()
+        self.optimizer = None
+
+    def forward(self, input):
+        return self.model(input)
+
+    def add_optimizer(self):
+        self.optimizer = optim.Adam(self.parameters(), lr=self.learning_rate)
+
+class SingleLayerBCEModel(nn.Module):
+    def __init__(self, embedding_dims, learning_rate=10**-3):
+        super().__init__()
+        self.model = nn.Sequential(
+            nn.Linear(embedding_dims, 1024),
+            nn.BatchNorm1d(1024),
+            nn.ReLU(),
+            nn.Linear(1024, 1),
+            nn.Sigmoid())
+        self.learning_rate = learning_rate
+        self.loss = nn.BCELoss()
+        self.optimizer = None
+
+    def forward(self, input):
+        return self.model(input)
+
+    def add_optimizer(self):
+        self.optimizer = optim.Adam(self.parameters(), lr=self.learning_rate)
